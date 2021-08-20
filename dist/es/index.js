bdfca84f68255dc6dbca07c818001b9150a63f72
Sebastian Pape <s@pape.io>
2021-08-20 14:04:20 +0200

    v4.2.3: fix dependency globals

--- dist/es/index.js
+++ dist/es/index.js
@@ -1,5 +1,3 @@
-
-(function(l, r) { if (l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (window.location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.getElementsByTagName('head')[0].appendChild(r) })(window.document);
 import { Blockchain } from 'depay-web3-blockchains';
 import { CONSTANTS } from 'depay-web3-constants';
 import require$$0 from 'buffer';
--- rollup.globals.js
+++ rollup.globals.js
@@ -1,4 +1,5 @@
 export default {
   'depay-web3-blockchains': 'Web3Blockchains',
-  'depay-web3-constants': 'Web3Constants'
+  'depay-web3-constants': 'Web3Constants',
+  'depay-web3-client': 'Web3Client',
 }
