343fdcb44c84a49f39a86597f1733ce028695700
Sebastian Pape <s@pape.io>
2021-09-20 16:51:05 +0200

    fix tests and package

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import EVMWallet from './wallets/EVMWallet'
 import MetaMask from './wallets/MetaMask'
+import Coinbase from './wallets/Coinbase'
 import Wallet from './wallets/Wallet'
 
 let getWallet = function () {
   if (typeof window.ethereum !== 'undefined') {
     if (typeof window.ethereum === 'object' && window.ethereum.isMetaMask) {
       return new MetaMask()
+    } else if (typeof window.ethereum === 'object' && window.ethereum.isCoinbaseWallet) {
+      return new Coinbase()
     } else {
       return new EVMWallet()
     }
