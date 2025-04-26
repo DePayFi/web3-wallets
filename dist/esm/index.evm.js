c577c995065c89b799a0a65564ee5e1a19c45bec
Sebastian Pape <0xneo11@gmail.com>
2025-04-26 09:13:32 +0200

    generate siwe message

--- dist/esm/index.evm.js
+++ dist/esm/index.evm.js
@@ -1623,7 +1623,52 @@ class WalletLink {
 WalletLink.getConnectedInstance = getConnectedInstance;
 WalletLink.setConnectedInstance = setConnectedInstance;
 
-function _nullishCoalesce(lhs, rhsFn) { if (lhs != null) { return lhs; } else { return rhsFn(); } } function _optionalChain$2(ops) { let lastAccessLHS = undefined; let value = ops[0]; let i = 1; while (i < ops.length) { const op = ops[i]; const fn = ops[i + 1]; i += 2; if ((op === 'optionalAccess' || op === 'optionalCall') && value == null) { return undefined; } if (op === 'access' || op === 'optionalAccess') { lastAccessLHS = value; value = fn(value); } else if (op === 'call' || op === 'optionalCall') { value = fn((...args) => value.call(lastAccessLHS, ...args)); lastAccessLHS = undefined; } } return value; }// https://github.com/worldcoin/minikit-js/blob/main/packages/core/helpers/transaction/validate-payload.ts
+function _nullishCoalesce(lhs, rhsFn) { if (lhs != null) { return lhs; } else { return rhsFn(); } } function _optionalChain$2(ops) { let lastAccessLHS = undefined; let value = ops[0]; let i = 1; while (i < ops.length) { const op = ops[i]; const fn = ops[i + 1]; i += 2; if ((op === 'optionalAccess' || op === 'optionalCall') && value == null) { return undefined; } if (op === 'access' || op === 'optionalAccess') { lastAccessLHS = value; value = fn(value); } else if (op === 'call' || op === 'optionalCall') { value = fn((...args) => value.call(lastAccessLHS, ...args)); lastAccessLHS = undefined; } } return value; }// https://github.com/worldcoin/minikit-js/blob/main/packages/core/helpers/siwe/siwe.ts
+const generateSiweMessage = (siweMessageData) => {
+  let siweMessage = '';
+
+  if (siweMessageData.scheme) {
+    siweMessage += `${siweMessageData.scheme}://${siweMessageData.domain} wants you to sign in with your Ethereum account:\n`;
+  } else {
+    siweMessage += `${siweMessageData.domain} wants you to sign in with your Ethereum account:\n`;
+  }
+
+  // NOTE: This differs from the ERC-4361 spec where the address is required
+  if (siweMessageData.address) {
+    siweMessage += `${siweMessageData.address}\n`;
+  } else {
+    siweMessage += '{address}\n';
+  }
+  siweMessage += '\n';
+
+  if (siweMessageData.statement) {
+    siweMessage += `${siweMessageData.statement}\n`;
+  }
+
+  siweMessage += '\n';
+
+  siweMessage += `URI: ${siweMessageData.uri}\n`;
+  siweMessage += `Version: ${siweMessageData.version}\n`;
+  siweMessage += `Chain ID: ${siweMessageData.chain_id}\n`;
+  siweMessage += `Nonce: ${siweMessageData.nonce}\n`;
+  siweMessage += `Issued At: ${siweMessageData.issued_at}\n`;
+
+  if (siweMessageData.expiration_time) {
+    siweMessage += `Expiration Time: ${siweMessageData.expiration_time}\n`;
+  }
+
+  if (siweMessageData.not_before) {
+    siweMessage += `Not Before: ${siweMessageData.not_before}\n`;
+  }
+
+  if (siweMessageData.request_id) {
+    siweMessage += `Request ID: ${siweMessageData.request_id}\n`;
+  }
+
+  return siweMessage
+};
+
+// https://github.com/worldcoin/minikit-js/blob/main/packages/core/helpers/transaction/validate-payload.ts
 const isValidHex = (str) => {
   return /^0x[0-9A-Fa-f]+$/.test(str)
 };
--- src/wallets/WorldApp/index.js
+++ src/wallets/WorldApp/index.js
@@ -1,3 +1,48 @@
+// https://github.com/worldcoin/minikit-js/blob/main/packages/core/helpers/siwe/siwe.ts
+const generateSiweMessage = (siweMessageData) => {
+  let siweMessage = ''
+
+  if (siweMessageData.scheme) {
+    siweMessage += `${siweMessageData.scheme}://${siweMessageData.domain} wants you to sign in with your Ethereum account:\n`
+  } else {
+    siweMessage += `${siweMessageData.domain} wants you to sign in with your Ethereum account:\n`
+  }
+
+  // NOTE: This differs from the ERC-4361 spec where the address is required
+  if (siweMessageData.address) {
+    siweMessage += `${siweMessageData.address}\n`
+  } else {
+    siweMessage += '{address}\n'
+  }
+  siweMessage += '\n'
+
+  if (siweMessageData.statement) {
+    siweMessage += `${siweMessageData.statement}\n`
+  }
+
+  siweMessage += '\n'
+
+  siweMessage += `URI: ${siweMessageData.uri}\n`
+  siweMessage += `Version: ${siweMessageData.version}\n`
+  siweMessage += `Chain ID: ${siweMessageData.chain_id}\n`
+  siweMessage += `Nonce: ${siweMessageData.nonce}\n`
+  siweMessage += `Issued At: ${siweMessageData.issued_at}\n`
+
+  if (siweMessageData.expiration_time) {
+    siweMessage += `Expiration Time: ${siweMessageData.expiration_time}\n`
+  }
+
+  if (siweMessageData.not_before) {
+    siweMessage += `Not Before: ${siweMessageData.not_before}\n`
+  }
+
+  if (siweMessageData.request_id) {
+    siweMessage += `Request ID: ${siweMessageData.request_id}\n`
+  }
+
+  return siweMessage
+};
+
 // https://github.com/worldcoin/minikit-js/blob/main/packages/core/helpers/transaction/validate-payload.ts
 const isValidHex = (str) => {
   return /^0x[0-9A-Fa-f]+$/.test(str)
